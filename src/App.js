e37d9600338d0c13cca5dc00689429ea1ad93378
wangshangzhe <wangshanghzhe@didiglobal.com>
2022-05-20 22:11:08 +0800

    feat: react.memo

--- src/App.js
+++ src/App.js
@@ -1,33 +1,63 @@
 import './App.css';
-import { useEffect, useState } from 'react';
-import MonacoEditor from 'react-monaco-editor';
-function App() {
-  const Updates = (count) => {
-    return <div>更新次数：{count}</div>
-  }
+import React,  { useEffect, useState ,useRef } from 'react';
 
-  const LeftCom = React.memo(()=>{
-    let leftRef = React.ref(0)
-    return <Updates count={leftRef.current++}></Updates>
-  })
+const Updates = ({ count }) => (
+  <div>更新次数：{ count }</div>
+)
 
-  const RightCom = ()=>{
-    let rightRef = React.ref(0)
-    return <Updates count={rightRef.current++}></Updates>
+const MemoCom = React.memo(()=>{
+  const leftRef = useRef(0)
+  return <div className='leftCom'>
+    <div>memo:</div>
+    <Updates count={leftRef.current++} />
+  </div>
+})
+
+// const MemoCom = React.memo(()=>{
+//   let leftRef = useRef(0)
+//   return <Updates count={leftRef.current++}></Updates>
+// }, (prevProps, nextProps) => {
+//   return prevProps.data.name === nextProps.data.name
+// })
+
+const RightCom = ()=>{
+  const rightRef = useRef(0)
+  return <div className='rightCom'>
+    <div>no-memo:</div>
+    <Updates count={rightRef.current++} />
+  </div>
+  
+}
+
+const App = () => {
+  const foo = 'foo'
+  const data = {name:'memo'}
+  // const data = React.memo(()=>({
+  //   name:'memo'
+  // }), [])
+  const testFn = ()=>{
+    console.log('testFn')
   }
+  // const testFn = useCallback(()=>{
+  //   console.log('testFn')
+  // },[])
 
   const [text, setText] = useState('')
-  let updates = React.ref(0)
+  const updates = useRef(0)
 
   useEffect(()=>{
     updates.current++
   })
   return (
-    
     <div className="App">
-      <Updates count={updates.current++}></Updates>
-      <input value={text} placeholder='请输入' onChange={(e) => setText(e.target.value)}></input>
-
+      <div>
+        <Updates count={updates.current} />
+      </div>
+      <input value={text} placeholder="请输入" onChange={(e) => setText(e.target.value)} />
+      <div>
+        <MemoCom foo={foo} />
+        <RightCom />
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -5,10 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log('root')
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
